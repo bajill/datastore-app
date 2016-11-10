96b5dde1da46a8d2eb031b75b3a0e4c06b655bf9
Morten Johannessen <mortenfjoh@gmail.com>
2016-11-10 21:29:31 +0100

    Created a python script to generate mockdata in both datastore and userdatastore, also updated README abit

---
+++ mockdata.py
@@ -0,0 +1,62 @@
+"""
+mockdata.py is created to be used in the final project of INF5750 Open Source Development.
+It fills up both the datastore and the userdatastore with mockdata so there
+are some more data to work with when we develop our final project.
+"""
+
+import sys
+import requests
+import json
+import time
+from requests.auth import HTTPBasicAuth
+
+headers = {"Content-type": "application/json"}
+
+def doRequest(reqURL, payload):
+    r = requests.put(reqURL, data=json.dumps(payload), auth=HTTPBasicAuth('admin', 'district'), headers=headers)
+    print r.status_code
+
+def generatePayload():
+    payload = {"first_name": "alex", "last_name": "jones"}
+    return payload
+
+def generateNamespace():
+    namespaces = ["arne/key", "kjetil/key", "per/key", "turid/key", "alfred/key", "joakim/key", "berit/key",
+            "arne/key2", "kjetil/key2", "per/key2", "turid/key2", "alfred/key2", "joakim/key2", "berit/key2",
+            "arne/key3", "kjetil/key3", "per/key3", "turid/key3", "alfred/key3", "joakim/key3", "berit/key3"]
+    return namespaces
+
+def mockDataStore():
+    dataStoreURL = "https://play.dhis2.org/dev/api/25/dataStore/"
+    namespaces = generateNamespace()
+
+    for name in namespaces:
+        dataStoreURL += name
+        payload = generatePayload()
+        doRequest(dataStoreURL, payload)
+        dataStoreURL = "https://play.dhis2.org/dev/api/25/dataStore/"
+        
+
+def mockUserDataStore():
+    userDataStoreURL = "https://play.dhis2.org/dev/api/25/userDataStore/"
+    namespaces = generateNamespace()
+
+    for name in namespaces:
+        userDataStoreURL += name
+        payload = generatePayload()
+        doRequest(userDataStoreURL, payload)
+        userDataStoreURL = "https://play.dhis2.org/dev/api/25/userDataStore/"
+
+
+if len(sys.argv) != 2:
+    print "[USAGE]: No argument, specify if you want userDataStore, dataStore or both. --ds --uds --both"
+
+if sys.argv[1] == "--ds":
+    mockDataStore()
+elif sys.argv[1] == "--uds":
+    mockUserDataStore()
+elif sys.argv[1] == "--both":
+    mockDataStore()
+    mockUserDataStore()
+else:
+    print "Not a valid argument"
